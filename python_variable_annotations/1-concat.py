f5e58eed04a300eb9c057280919dc28be12ed8a1
ajokkuechajokdeng <a.deng2@alustudent.com>
2024-09-07 01:32:59 +0200

    Python - Variable Annotations

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -10,3 +10,4 @@ def concat(str1: str, str2: str) -> str:
     A function that returns a concatenated string
     """
     return str1 + str2
+
--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -10,3 +10,4 @@ def floor(n: float) -> int:
     A function that returns the floor value of a float
     """
     return int(n)
+
